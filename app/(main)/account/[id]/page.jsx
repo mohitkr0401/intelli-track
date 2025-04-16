5867886adf5c64f50670073b614c4a0fbdfbdf3a
mohitkr0401 <mohitkr0401@gmail.com>
2025-04-17 04:38:54 +0530

    feat: Add TransactionTable component and default categories for income and expenses

--- app/(main)/account/[id]/page.jsx
+++ app/(main)/account/[id]/page.jsx
@@ -1,6 +1,8 @@
 import { getAccountWithTransactions } from "@/actions/accounts";
 import { notFound } from "next/navigation";
-import React from "react";
+import React, { Suspense } from "react";
+import { BarLoader } from "react-spinners";
+import TransactionTable from "../_components/transaction-table";
 
 const AccountsPage = async ({ params }) => {
   const accountData = await getAccountWithTransactions(params.id);
@@ -25,13 +27,26 @@ const AccountsPage = async ({ params }) => {
 
         <div className="text-right pb-2">
           <div className="text-xl sm:text-2xl font-bold">
-          ₹{parseFloat(account.balance).toFixed(2)}
+            ₹{parseFloat(account.balance).toFixed(2)}
           </div>
           <p className="text-sm text-muted-foreground">
             {account._count.transactions} Transactions
           </p>
         </div>
       </div>
+      {/* Chart Section */}
+      {/* <Suspense
+        fallback={<BarLoader className="mt-4" width={"100%"} color="#9333ea" />}
+      >
+        <AccountChart transactions={transactions} />
+      </Suspense> */}
+
+      {/* Transactions Table */}
+      <Suspense
+        fallback={<BarLoader className="mt-4" width={"100%"} color="#9333ea" />}
+      >
+        <TransactionTable transactions={transactions} />
+      </Suspense>
     </div>
   );
 };
