f5961910a45c2e94a6cbeca251913d77d4218290
mohitkr0401 <mohitkr0401@gmail.com>
2025-04-30 01:34:57 +0530

    feat: Implement budget progress component and update budget functionality; enforce unique userId constraint in Budget model

--- app/(main)/dashboard/page.jsx
+++ app/(main)/dashboard/page.jsx
@@ -2,15 +2,28 @@ import CreateAccountDrawer from "@/components/create-account-drawer";
 import { Card, CardContent } from "@/components/ui/card";
 import { Plus } from "lucide-react";
 import React from "react";
-import { getUserAccounts } from '@/actions/dashboard';
+import { getUserAccounts } from "@/actions/dashboard";
 import AccountCard from "./_components/account-card";
+import { BudgetProgress } from "./_components/budget-progress";
+import { getCurrentBudget } from "@/actions/budget";
 
 async function DashboardPage() {
   const accounts = await getUserAccounts();
+  const defaultAccount = accounts?.find((account) => account.isDefault);
+  // Get budget for default account
+  let budgetData = null;
+  if (defaultAccount) {
+    budgetData = await getCurrentBudget(defaultAccount.id);
+  }
   return (
     <div className="px-5">
       {/* Budget Progress */}
-
+      {defaultAccount && (
+        <BudgetProgress
+          initialBudget={budgetData?.budget}
+          currentExpenses={budgetData?.currentExpenses || 0}
+        />
+      )}
       {/* Overview */}
 
       {/* Accounts Grid */}
@@ -18,15 +31,16 @@ async function DashboardPage() {
         <CreateAccountDrawer>
           <Card className="hover:shadow-md transition-shadow cursor-pointer border-dashed">
             <CardContent className="flex flex-col items-center justify-center text-muted-foreground h-full pt-5">
-              <Plus className="h-10 w-10 mb-2"/>
+              <Plus className="h-10 w-10 mb-2" />
               <p className="text-sm font-medium">Add New Account</p>
             </CardContent>
           </Card>
         </CreateAccountDrawer>
 
-        {accounts.length >0 && accounts?.map((account)=>{
-          return <AccountCard key={account.id} account={account} />;
-        }) }
+        {accounts.length > 0 &&
+          accounts?.map((account) => {
+            return <AccountCard key={account.id} account={account} />;
+          })}
       </div>
     </div>
   );
