233d8a6937c21f2c629697b20465285a7d3f2fb0
mohitkr0401 <mohitkr0401@gmail.com>
2025-05-01 00:57:44 +0530

    feat: Add Inngest dependency to package.json and package-lock.json

--- app/api/inngest/route.js
+++ app/api/inngest/route.js
@@ -1,13 +1,12 @@
 import { serve } from "inngest/next";
 
 import { inngest } from "@/lib/inngest/client";
+import { checkBudgetAlerts } from "@/lib/inngest/functions";
 
 export const { GET, POST, PUT } = serve({
   client: inngest,
   functions: [
-    processRecurringTransaction,
-    triggerRecurringTransactions,
-    generateMonthlyReports,
+    // Add your functions here
     checkBudgetAlerts,
   ],
 });
