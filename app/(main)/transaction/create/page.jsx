62464ee0d3f280d64ea1399da0aa9445b24a5e08
mohitkr0401 <mohitkr0401@gmail.com>
2025-05-02 17:42:36 +0530

    feat: Create AddTransactionPage component to handle transaction addition

---
+++ app/(main)/transaction/create/page.jsx
@@ -0,0 +1,22 @@
+import { getUserAccounts } from '@/actions/dashboard'
+import { defaultCategories } from '@/data/categories';
+import React from 'react'
+
+const AddTransactionPage = async () => {
+    const accounts = await getUserAccounts();
+  return (
+    <div className="max-w-3xl mx-auto px-5">
+      <div className="flex justify-center md:justify-normal mb-8">
+        <h1 className="text-5xl gradient-title ">Add Transaction</h1>
+      </div>
+      <AddTransactionForm
+        accounts={accounts}
+        categories={defaultCategories}
+        editMode={!!editId}
+        initialData={initialData}
+      />
+    </div>
+  )
+}
+
+export default AddTransactionPage
